4c2acbec87ac79043ef0a770bedb306814d4bb8e
Julian Andres Espinosa Torres <deimy.godoy@utp.edu.co>
2023-11-06 21:04:13 -0500

    prueba

--- src/torneofutbol/TorneoFutbol.java
+++ src/torneofutbol/TorneoFutbol.java
@@ -17,6 +17,7 @@ public class TorneoFutbol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Holaaaaaa");
     }
     
 }
