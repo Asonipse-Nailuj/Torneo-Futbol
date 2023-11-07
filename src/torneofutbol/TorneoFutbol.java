638556c7308ec2f946dddc2de14ccca79d8256e3
Oscar Omar Moreno Cruz <150073419+FrontendColombia@users.noreply.github.com>
2023-11-06 21:37:33 -0500

    Cambio HELLO

--- src/torneofutbol/TorneoFutbol.java
+++ src/torneofutbol/TorneoFutbol.java
@@ -16,7 +16,7 @@ public class TorneoFutbol {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hello");
     }
     
 }
